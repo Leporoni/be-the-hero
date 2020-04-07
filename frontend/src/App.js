5b8320dca566f1a65c4834069bd9986b05942662
Leporoni <leporoni@hotmail.com>
2020-04-07 13:07:02 -0300

    Atualizando e limpando estrutura

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello OmniStack Week - Lets Grow Up Toghe </p>
-                <a className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer" >
-                    Learn React </a>
-            </header>
-        </div>
+       <Header>
+           Legião da Boa Vontade!!!
+       </Header>
     );
 }
 
