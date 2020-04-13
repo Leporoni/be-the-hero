24917f12b4d03a71f3d0673d8ae051dbf17c9df4
Leporoni <leporoni@hotmail.com>
2020-04-13 15:28:44 -0300

    Criando CSS na pagina de Logon e Global

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
+//import { Link, useHistory } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
+
+//import api from '../../services/api'
+
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
-import { FiLogIn } from 'react-icons/fi';
+
 import './styles.css'
 
 export default function Logon() {
     return (
-       <div className="logon-container">
-           <section className="form">
+        <div className="logon-container">
+            <section className="form">
                 <img src={logoImg} alt="Be The Hero" />
 
                 <form>
@@ -16,12 +21,16 @@ export default function Logon() {
                     <input placeholder="Your Id" />
                     <button className="button" type="submit">Enter</button>
 
+                    <a href="/register">
+                        <FiLogIn size={16} color="#e02041" />
+                        Create an account
+                    </a>
 
                 </form>
-           </section>
+            </section>
 
-           <img src={heroesImg} alt="Heroes" />
+            <img src={heroesImg} alt="Heroes" />
 
-       </div>
+        </div>
     );
 }
